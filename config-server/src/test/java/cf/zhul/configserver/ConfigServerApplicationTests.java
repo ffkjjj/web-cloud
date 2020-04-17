048fb83e5dd2426a36c77d37e53098eba14a67be
zhulei <zhulei@isurpass.com.cn>
2020-04-17 11:06:46 +0800

    加入配置中心

--- config-server/src/test/java/cf/zhul/configserver/ConfigServerApplicationTests.java
+++ config-server/src/test/java/cf/zhul/configserver/ConfigServerApplicationTests.java
@@ -1,6 +1,6 @@
 package cf.zhul.configserver;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
--- service-feign/src/main/java/cf/zhul/servicefeign/controller/HelloController.java
+++ service-feign/src/main/java/cf/zhul/servicefeign/controller/HelloController.java
@@ -1,6 +1,7 @@
 package cf.zhul.servicefeign.controller;
 
 import cf.zhul.servicefeign.service.IHelloService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +10,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 
     private final IHelloService helloService;
+
+    @Value("${version}")
+    private String version;
 
     public HelloController(IHelloService helloService) {
         this.helloService = helloService;
@@ -19,4 +23,8 @@ public class HelloController {
         return helloService.sayHelloFromServiceHi(name);
     }
 
+    @RequestMapping(value = "/version")
+    public String version() {
+        return version;
+    }
 }
