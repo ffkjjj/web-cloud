73dcddbe8037b4fc03fb81df0b646cf40974a091
zhulei <zhulei@isurpass.com.cn>
2020-04-17 20:04:45 +0800

    更换网关为 spring cloud gateway

---
+++ service-gateway/src/test/java/cf/zhul/servicegateway/ServiceGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package cf.zhul.servicegateway;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
